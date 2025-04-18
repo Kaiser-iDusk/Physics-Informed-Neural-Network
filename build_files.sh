26c8ee87b554d9b81126f8db44122b5555774325
Shirsendu <palshirsendu03@gmail.com>
2025-04-18 12:54:42 +0530

    Made modifications

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,11 @@
-python3.9 -m venv myenv
+# create venv
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
+# install requirements
 pip install -r requirements.txt
+
+# build static
 python3.9 manage.py collectstatic --noinput
