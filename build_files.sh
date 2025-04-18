89f591c74da72f27c6b9ef0939a37d459171a4a6
Shirsendu <palshirsendu03@gmail.com>
2025-04-18 12:57:50 +0530

    Made modifications

--- build_files.sh
+++ build_files.sh
@@ -4,6 +4,9 @@ python3.9 -m venv venv
 # activate the virtual environment
 source venv/bin/activate
 
+# upgrade pip
+/vercel/path0/venv/bin/python3.9 -m pip install --upgrade pip
+
 # install requirements
 pip install -r requirements.txt
 
